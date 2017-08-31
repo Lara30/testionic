bfbe608ff42b485ce4202ea10a24254e2d07ba7f
Lara30 <havardnadine@yahoo.fr>
2017-08-31 15:15:27 +0200

    jeudi

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage{
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,private geolocation:Geolocation){
   }
-
+  load() {
+    //get current position
+      this.geolocation.getCurrentPosition().then((pos) => {
+        console.log('lat:'+pos.coords.latitude +',lon:'+pos.coords.longitude);
+      });
+      //to stop watching
+  }
 }
